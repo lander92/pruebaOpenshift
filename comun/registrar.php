c6466e5738164666d8bc6224584fae57652b7186
unknown <landerreyes92@gmail.com>
2014-10-30 11:45:22 +0100

    cambios registrar

--- comun/registrar.php
+++ comun/registrar.php
@@ -2,9 +2,9 @@
 if(!empty($_POST['dni'])){
 	//incluimos el archivo conexion.php para utilizar sus funciones
 	include '../conexion.php';
-	$conexion=conectar();
+	conectar();
 	selecDb();
-	//guardo en variables el nombre de la bd y de la tabla que voy a utilizar
+	//guardo en variable el nombre de la tabla que voy a utilizar
 	$dbTabla="usuarios";
 	//guardo en variables los datos que recojo del formulario
 	$dni=$_POST['dni'];
@@ -17,9 +17,8 @@ if(!empty($_POST['dni'])){
 	//guardo en $consulta la consulta que voy a ejecutar
 	$consulta="INSERT INTO $dbTabla (dni,nombre,apellidos,password,localidad,telefono,tipo) 
 			VALUES ('$dni','$nombre','$apellidos','$password','$localidad','$telefono','$tipo')";
-	//$result = mysql_query($consulta) or die(mysql_error());
 	//ejecutamos la consulta
-	if(mysql_query($consulta,$conexion)){
+	if(mysql_query($consulta)){
 		//si se ejecuta correctamente
 		header ("location: ../index.php");
 	}else{
@@ -32,3 +31,4 @@ if(!empty($_POST['dni'])){
 
 //cerramos la conexion a la base de datos
 mysql_close();
+?>
